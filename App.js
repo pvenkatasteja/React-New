2622c8986cf29d199db001cb3c388ee578b16ac4
pvenkatasteja <venkatasaiteja187@gmail.com>
2024-09-04 23:34:08 +0530

    Up to date

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm an h1 tag "),
-        React.createElement("h2", {}, "I'm an h2 tag"),
+import React from "react";
+import ReactDOM from "react-dom";
 
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    ]),
-])
+const RestaurantCard =() =>{
+  return(
+    <div className="restaurant-card" style={{backgroundColor:"#f0f0f0"}}>
+      <img 
+      className="restaurant-logo"
+      alt="" 
+      src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani,South India, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+const Body = () =>{
+  return(
+    <div className="body">
+    <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard />
+      </div>
+    
+  </div>
+  )
+  
+}
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<AppLayout />);
